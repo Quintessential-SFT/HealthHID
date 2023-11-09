0b5e2a2256aa5f2ae58cb29cd460cb1795aeca4f
Konstantinos Feretos <konferetos@tutanota.com>
2023-11-09 16:31:25 +0200

    fix: .d.ts file generation

--- rspack.config.js
+++ rspack.config.js
@@ -1,24 +1,35 @@
 const path = require('path');
 const rspack = require('@rspack/core');
+const NpmDtsPlugin = require('npm-dts-webpack-plugin')
 
-module.exports = (env, exports) => ({
-  context: __dirname,
-  mode: !!env.production ? 'production' : 'development',
-  devtool: !!env.production ? 'source-map' : false,
-  entry: {
-    main: './lib/index.ts',
-  },
-  output: {
-    filename: 'health-hid.js',
-    path: path.resolve(__dirname, 'dist'),
-    library: {
-      name: 'HealthHid',
-      type: 'umd',
+const prodPlugins = [
+  new NpmDtsPlugin({
+    output: 'dist/health-hid.d.ts',
+  }),
+];
+
+module.exports = (env, exports) => {
+  const production = env.production;
+  return {
+    context: __dirname,
+    mode: production ? 'production' : 'development',
+    devtool: production ? 'source-map' : false,
+    entry: {
+      main: './lib/index.ts',
     },
-  },
-  plugins: [
-    new rspack.HtmlRspackPlugin({
-      template: './src/index.html',
-    }),
-  ],
-});
+    output: {
+      filename: 'health-hid.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: {
+        name: 'HealthHid',
+        type: 'umd',
+      },
+    },
+    plugins: [
+      new rspack.HtmlRspackPlugin({
+        template: './src/index.html',
+      }),
+      ...(production ? prodPlugins : []),
+    ],
+  };
+};
